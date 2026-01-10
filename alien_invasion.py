73fad12dc7e35db0cba29675f2752c0e08a6da09
Ahad <mohdahad60080@gmail.com>
2026-01-10 17:31:13 +0530

    cleaner event loop

--- alien_invasion.py
+++ alien_invasion.py
@@ -16,15 +16,19 @@ class AlienInvasion:
     def run_game(self):
         while True:
             self.clock.tick(60)
+            self._update_screen()
             self._check_events()
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-            pygame.display.flip()
 
     def _check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        pygame.display.flip()
+
 
 
 if __name__ == '__main__':
