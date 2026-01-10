518b4d9feb849d6fe6ebcd505a39913ad51e3875
Ahad <mohdahad60080@gmail.com>
2026-01-10 18:57:05 +0530

    speed up

--- alien_invasion.py
+++ alien_invasion.py
@@ -42,7 +42,7 @@ class AlienInvasion:
         pygame.display.flip()
 
 
-
 if __name__ == '__main__':
     ai = AlienInvasion()
     ai.run_game()
+
--- settings.py
+++ settings.py
@@ -3,4 +3,5 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (245, 245, 220)
+        self.ship_speed = 5
         pass
